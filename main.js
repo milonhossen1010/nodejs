165412286a5d084584e683b239e2904bf2b9f32b
milon <milonhossen1010@gmail.com>
2022-07-17 07:51:03 +0600

    2 no class

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
 const http = require("http");
 
 let server = http.createServer(function (req, res) {
-res.end("<h1>Hello world!</h1>");
+    if (req.url == "/"){
+        res.writeHead(200, {"Content-Type" : "text/html"});
+        res.write("<h1>This is home page</h1>");
+        res.end();
+    }
+
+    else if (req.url == "/about"){
+        res.writeHead(200, {"Content-Type" : "text/html"});
+        res.write("<h1>This is About us page</h1>");
+        res.end();
+    }
+
+    else if (req.url == "/contact"){
+        res.writeHead(200, {"Content-Type" : "text/html"});
+        res.write("<h1>This is contact us page</h1>");
+        res.end();
+    }
+
 });
 
 server.listen(5050);
+
 console.log("server is running");
